4e8ed7b9ff57ad162f7254bbfba0bdaca3956b02
Ryanwalker277 <andianurag277@gmail.com>
2023-05-14 13:51:38 +0530

    Fixed login

--- initdb/init.sh
+++ initdb/init.sh
@@ -3,9 +3,8 @@ set -e
 
 echo "Running test script"
 
-psql -v ON_ERROR_STOP=1 --username "aggregate" <<-EOSQL
-    \connect aggregate;
-    CREATE SCHEMA aggregate;
-    ALTER SCHEMA aggregate OWNER TO aggregate;
-    GRANT ALL PRIVILEGES ON SCHEMA aggregate TO aggregate;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA "$POSTGRES_DB";
+    ALTER SCHEMA "$POSTGRES_DB" OWNER TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON SCHEMA "$POSTGRES_DB" TO "$POSTGRES_USER";
 EOSQL
